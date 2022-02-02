954120d7066fb24f890e5b728d2bb4ebceb76562
Beks <guess777@gmail.com>
2022-02-01 20:51:34 -0500

    Base Routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import { Route, Routes } from "react-router";
+
+import HomeFeed from "./Components/HomeFeed";
+import Notifications from "./Components/Notifications";
+import Bookmarks from "./Components/Bookmarks";
+import TweetDetails from "./Components/TweetDetails";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Hello World!</div>
+      <Routes>
+        <Route exact path="/" element={<HomeFeed />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
+        <Route path="/tweet/:tweetId" element={<TweetDetails />} />
+        <Route path="/:profileId" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
